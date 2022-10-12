62919aaa610c129822dfd46d750f1f5b1dbe3e72
wuzhixuan <jackwu@java.com>
2022-10-13 00:41:10 +0800

    feat: 账号微服务增加阿里云短信sdk

--- cloud-account/src/main/java/com/wuzx/component/SmsComponent.java
+++ cloud-account/src/main/java/com/wuzx/component/SmsComponent.java
@@ -1,5 +1,11 @@
 package com.wuzx.component;
 
+import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
+import com.aliyun.tea.TeaException;
+import com.aliyun.teaopenapi.models.Config;
+import com.aliyun.teautil.models.RuntimeOptions;
 import com.wuzx.config.SmsConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +61,70 @@ public class SmsComponent {
 
     }
 
+
+
+    /**
+     * 使用AK&SK初始化账号Client
+     * @param accessKeyId
+     * @param accessKeySecret
+     * @return Client
+     * @throws Exception
+     */
+    public static Client createClient(String accessKeyId, String accessKeySecret) throws Exception {
+        Config config = new Config()
+                // 您的 AccessKey ID
+                .setAccessKeyId(accessKeyId)
+                // 您的 AccessKey Secret
+                .setAccessKeySecret(accessKeySecret)
+                // 访问的域名
+                .setEndpoint("dysmsapi.aliyuncs.com");
+
+        return new Client(config);
+    }
+
+
+    /**
+     * 阿里云发送短信验证码
+     *
+     * @param PhoneNumbers 接收短信的手机号码
+     * @param templateCode 短信模板CODE
+     * @param value
+     */
+    public static void sendAli(String PhoneNumbers, String templateCode, String value) {
+
+        // 短信签名名称
+        String SignName = "JAVA学习95";
+
+
+        try {
+            final Client client = createClient("xxx", "xxx");
+            final SendSmsRequest sendSmsRequest = new SendSmsRequest()
+                    .setSignName(SignName)
+                    .setTemplateCode(templateCode)
+                    .setTemplateParam(String.format("{\"code\":\"%s\"}", value))
+                    .setPhoneNumbers(PhoneNumbers);
+
+
+            RuntimeOptions runtime = new RuntimeOptions();
+            // 复制代码运行请自行打印 API 的返回值
+            final SendSmsResponse sendSmsResponse = client.sendSmsWithOptions(sendSmsRequest, runtime);
+
+
+        }catch (TeaException error) {
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(error.message);
+        } catch (Exception _error) {
+            TeaException error = new TeaException(_error.getMessage(), _error);
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(error.message);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        sendAli("18702623606","SMS_173251911","666666");
+    }
 
 
 
